7add7b479ef1212b40ee28a51405712d518a6713
Yallison Melo <yallisonfelipe@outlook.com>
2018-08-02 00:21:24 -0300

    Acesso ao Banco de Dados Postgres

--- routes/produto.js
+++ routes/produto.js
@@ -1,5 +1,16 @@
 module.exports = function(app){
     app.get('/produtos', function(req, res) {
-        res.render("produtos/lista");
+        const pg = require('pg');
+        const pool = new pg.Pool({
+        user: 'postgres',
+        host: '127.0.0.1',
+        database: 'testenode',
+        password: '1991',
+        port: '5432'});
+        
+        pool.query("SELECT * FROM LIVRO", (err, result) => {
+        res.send(result);
+        pool.end();
+        });
     });
 }
